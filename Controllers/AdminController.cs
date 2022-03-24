cbfdaabda2cb0bd93265f2c381e771aa4536d9c3
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-03-23 22:12:58 -0600

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Temple.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Temple.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AppointmentContext context { get; set; }
+
+        public AdminController(AppointmentContext temp) => context = temp;
+
+        public IActionResult Appointment()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ViewAppointments()
+        {
+            var blah = context.Appointment
+                .Where(x => x.Available == false)
+                .OrderBy(x => x.Date)
+                .ToList();
+           
+
+            return View(blah);
+        }
+   
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var appointment = context.Appointment.Single(x => x.AppointmentId == id);
+            return View("Appointment", appointment);
+        }
+        [HttpPost]
+        public IActionResult Edit(Appointment apt)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(apt);
+                context.SaveChanges();
+
+                return RedirectToAction("ViewAppointments");
+            }
+            else
+            {
+                return View(apt);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var appointment = context.Appointment.Single(x => x.AppointmentId == id);
+
+            return View(appointment);
+        }
+        [HttpPost]
+        public IActionResult Delete(Appointment apt)
+        {
+            apt.Date = apt.Date;
+            apt.GroupSize = 0;
+            apt.GroupName = null;
+            apt.Email = null;
+            apt.Phone = null;
+            apt.Available = true;
+            context.Appointment.Update(apt);
+            context.SaveChanges();
+            return RedirectToAction("ViewAppointments");
+        }
+        [HttpPost]
+        public IActionResult Save()
+        {
+            context.SaveChanges();
+            return View("ViewAppointments");
+        }
+
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Temple.Models;
+
+namespace Temple.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAppointmentRepository repo { get; set; }
+
+        public HomeController(IAppointmentRepository temp)
+        {
+            repo = temp;
+        }
+
+        //private readonly ILogger<HomeController> _logger;
+        //private AppointmentContext blahContext { get; set; }
+
+        //public HomeController(ILogger<HomeController> logger, AppointmentContext someName)
+        //{
+        //    _logger = logger;
+        //    blahContext = someName;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            var appointmentList = repo.Appointments
+                .OrderBy(x => x.Date)
+                .Where(x => x.Available == true)
+                .ToList();
+
+            return View(appointmentList);
+        }
+
+        //public IActionResult ReservationForm()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        [HttpGet]
+        public IActionResult ReservationForm(int id)
+        {
+            var appointment = repo.Appointments.FirstOrDefault(x => x.AppointmentId == id);
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult ReservationForm(Appointment app)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.SaveAppointment(app);
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
